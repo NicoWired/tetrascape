543338bc2402162f478183ebc03c6d0cc774b57f
NicoWired <169010888+NicoWired@users.noreply.github.com>
2026-02-01 16:29:50 +0100

    add level background

--- source/levels/background/background.gd
+++ source/levels/background/background.gd
@@ -2,10 +2,9 @@ class_name Background
 extends Node2D
 
 @onready var polygon_2d: Polygon2D = $Polygon2D
-@onready var sprite_2d: Sprite2D = $Sprite2D
 @onready var texture_rect: TextureRect = $TextureRect
 
-# Called when the node enters the scene tree for the first time.
+
 func _ready() -> void:
 	polygon_2d.polygon = PackedVector2Array([
 		Vector2.ZERO,
@@ -13,5 +12,4 @@ func _ready() -> void:
 		Vector2(GameConfig.BOARD_SIZE.x * GameConfig.TILE_SIZE, GameConfig.BOARD_SIZE.y  * GameConfig.TILE_SIZE),
 		Vector2(0 ,GameConfig.BOARD_SIZE.y * GameConfig.TILE_SIZE)
 		])
-	#sprite_2d.region_rect.end = GameConfig.BOARD_SIZE * GameConfig.TILE_SIZE
-	texture_rect.size = GameConfig.BOARD_SIZE * GameConfig.TILE_SIZE
+	texture_rect.custom_minimum_size = GameConfig.BOARD_SIZE as Vector2 * GameConfig.TILE_SIZE * (Vector2.ONE / texture_rect.scale)
