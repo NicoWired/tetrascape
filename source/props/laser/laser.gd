e6fc226ad0ad019e1ded5ffc94cfe56f62ae0f16
NicoWired <169010888+NicoWired@users.noreply.github.com>
2026-02-13 23:39:24 +0100

    add laser fanning

--- source/props/laser/laser.gd
+++ source/props/laser/laser.gd
@@ -1,8 +1,16 @@
 extends Node2D
 
 @export var rotation_range: float = 30
+@export var fanning_time: float = 3
+
+var fanning_tween: Tween
 
 @onready var laser_beam: LaserBeam = $LaserBeam
 
 func _ready() -> void:
-	laser_beam.rotation_degrees += randf_range(0, rotation_range)
+	#laser_beam.rotation_degrees += randf_range(0, rotation_range)
+	fanning_tween = create_tween()
+	fanning_tween.tween_property(laser_beam, "rotation_degrees", rotation_range, fanning_time)
+	fanning_tween.tween_property(laser_beam, "rotation_degrees", 0, fanning_time)
+	fanning_tween.set_loops()
+	fanning_tween.play()
