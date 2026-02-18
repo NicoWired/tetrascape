16325ff1cb13921d1f7dfed0ce2d674e7c8ad3db
NicoWired <169010888+NicoWired@users.noreply.github.com>
2026-02-18 21:18:27 +0100

    tweak player values and split spikes into a different layer

--- source/player/playerconfig.gd
+++ source/player/playerconfig.gd
@@ -3,16 +3,16 @@ extends Resource
 
 # movement constants
 @export var values: Dictionary[StringName,float] = {
-	&"horizontal_acceleration": 1000,
+	&"horizontal_acceleration": 900,
 	&"horizontal_deceleration": 1500,
-	&"max_speed": 350,
+	&"max_speed": 320,
 	&"min_jump_velocity": -300,
-	&"jump_charge_time": 0.5,
+	&"jump_charge_time": 0.6,
 	&"jump_increment": 400,
 	&"wall_jump_x": 350,
 	&"wall_friction": 0.3,
-	&"coyote_time": 0.03,
-	&"fall_speed_multiplier": 1,
+	&"coyote_time": 0.04,
+	&"fall_speed_multiplier": 1.2,
 	&"base_size": 64,
 }
 
